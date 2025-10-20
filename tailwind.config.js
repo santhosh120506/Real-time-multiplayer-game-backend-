4ea5f27272caa294dd7b2118029bf27660cbbb19
Santhosh <santhoshkamarajv@gmail.com>
2025-10-20 19:00:54 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'game-primary': '#646cff',
+        'game-secondary': '#535bf2',
+      }
+    },
+  },
+  plugins: [],
+}
